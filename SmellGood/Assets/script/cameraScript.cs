5997e5360ce621d7d8a7ae72478ca4b161cce493
evakimox <zhangjn1121@gmail.com>
2019-03-08 21:19:12 -0500

    add wall and chichken

---
+++ SmellGood/Assets/script/cameraScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour {
+
+    private float cameraBoundary;
+
+	// Use this for initialization
+	void Start () {
+        cameraBoundary = 9;
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey("left shift") || Input.GetKey("right shift"))
+        {
+            cameraMovingListener();
+        }
+
+    }
+
+    void cameraMovingListener() 
+    {
+        if (Input.GetKey("right"))
+        {
+            Vector3 pos = transform.position;
+            pos.x = pos.x - 0.1f;
+            if (Mathf.Abs(pos.x) < cameraBoundary)
+            {
+                transform.position = pos;
+            }
+        }
+        if (Input.GetKey("left"))
+        {
+            Vector3 pos = transform.position;
+            pos.x = pos.x + 0.1f;
+            if (Mathf.Abs(pos.x) < cameraBoundary)
+            {
+                transform.position = pos;
+            }
+        }
+        if (Input.GetKey("up"))
+        {
+            Vector3 pos = transform.position;
+            pos.z = pos.z + 0.1f;
+            if (Mathf.Abs(pos.x) < cameraBoundary)
+            {
+                transform.position = pos;
+            }
+        }
+    }
+}
